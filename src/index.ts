75df38642fb8ad0ed6bcfafaa3ee7683e329f724
lucasliet <lucasouliveira@gmail.com>
2020-11-04 19:50:49 -0300

    init

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import { Client } from '@rmp135/imgur';
+import FileStream from 'fs';
+/**
+* Connect to Imgur API
+* @param client_id the client id on imgur token
+* @author Lucas Souza
+*/
+export default class Imgur{
+  private client_id:string;
+
+  constructor(client_id: string){
+    this.client_id = client_id;
+  }
+
+  /**
+   * Uploads a Image to Imgur asynchronous and returns the image url
+   * @param filepath filepath of the image file you want to upload
+   */
+  async upload(filepath: string){
+    const api = new Client({ client_id: this.client_id });
+    
+    const file_base64 = FileStream.readFileSync(filepath)
+      .toString('base64');
+    
+    const { data: { link } } = await api.Image.upload(
+      file_base64,
+      { 
+        type: 'base64',
+        title: filepath
+      }
+    );
+      
+    return link;
+  }
+}
