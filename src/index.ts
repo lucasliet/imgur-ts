ab14b2243e79c5bd920dc158de4778856c2a884b
lucasliet <lucasouliveira@gmail.com>
2020-11-05 15:40:20 -0300

    ⚙ build before publish

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import { Client } from '@rmp135/imgur';
 import FileStream from 'fs';
 /**
  * Uploads a Image to Imgur asynchronous and returns the image url
+ * @author Lucas Souza <lucasouliveira@gmail.com>
  * @param filepath filepath of the image file you want to upload
- * @author Lucas Souza
+ * @returns url of the uploaded image
 */
 export default async function upload(filepath: string): Promise<string>{
   const api = new Client({ client_id: '3fe85ea6714be32' });
